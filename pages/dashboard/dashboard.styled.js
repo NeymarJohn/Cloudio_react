149afa2a77e71b675fe96b74dddc9877048b73c1
Sunriver <seriousman1994217winter@gmail.com>
2023-06-28 07:11:32 +0400

    feat(:add)Add history content

--- pages/dashboard/dashboard.styled.js
+++ pages/dashboard/dashboard.styled.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styled, { css } from "styled-components";
 
 export const Title = styled.div`
@@ -36,6 +37,7 @@ export const HistoryNotifyText = styled.div`
 
 export const ValidNotifyText = styled.div`
   background: rgba(250, 103, 5, 1);
+  padding: 20px 40px;
   h1 {
     font-family: Rubik;
     font-size: 16px;
@@ -54,4 +56,70 @@ export const ValidNotifyText = styled.div`
     text-align: left;
     color: rgba(255, 255, 255, 1);
   }
+`;
+
+export const ContentRoot = styled.div`
+  padding: 0px 30px 30px 30px;
+  max-height: 400px;
+`;
+
+export const ContentItemBody = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 30px;
+`;
+
+export const HistoryInfo = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+export const HistoryImage = styled(Image)`
+  width: 30px;
+  height: 30px;
+`;
+
+export const HistoryContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+  h1 {
+    font-family: Rubik;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 22px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: rgba(43, 82, 221, 1);
+  }
+  h2 {
+    font-family: Rubik;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 21px;
+    letter-spacing: 0px;
+    text-align: left;
+    color: rgba(58, 64, 80, 1);
+  }
+  h3 {
+    //styleName: Body/Regular;
+    font-family: Rubik;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 21px;
+    letter-spacing: 0px;
+    text-align: left;
+    color: rgba(58, 64, 80, 1);
+  }
+`;
+
+export const ConfirmDate = styled.div`
+  font-family: Rubik;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 18px;
+  letter-spacing: 0px;
+  text-align: left;
+  color: rgba(153, 157, 169, 1);
 `;
